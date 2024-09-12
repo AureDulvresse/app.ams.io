e2e56f53f2f5cd14dfe8246ae9efca7bb016faba
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-12 19:59:50 +0100

    Update DashboardPage.tsx

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -6,9 +6,9 @@ import Sidebar from "@/components/partials/Sidebar";
 
 const DashboardPage : React.FC = () => {
   return (
-    <div className="flex h-screen">
+    <div className="flex min-h-screen">
       <Sidebar />
-      <div className="flex-1 flex flex-col px-4 py-2 bg-gray-100">
+      <div className="flex-1 flex flex-col px-4 py-2 bg-gray-100 ml-64">
         <Navbar />
         <main className="flex-1">
           <Dashboard />
