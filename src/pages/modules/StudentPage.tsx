1c61086fe0feec351fd54b6b3d6551fc2808a49d
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-16 10:16:40 +0100

    Update StudentPage.tsx

--- src/pages/modules/StudentPage.tsx
+++ src/pages/modules/StudentPage.tsx
@@ -7,7 +7,7 @@ const StudentPage: React.FC = () => {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <div className="flex-1 flex flex-col px-4 py-2 bg-gray-100 ml-64">
+      <div className="flex-1 flex flex-col px-4 py-2 bg-slate-50 dark:bg-gray-800 ml-64">
         <Navbar />
         <main className="flex-1">
           <Students />
