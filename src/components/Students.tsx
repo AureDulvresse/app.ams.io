1fc19f7fba3d1e245dc8e245d936776c8bb1e06f
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-18 11:40:04 +0100

    Update Students.tsx

--- src/components/Students.tsx
+++ src/components/Students.tsx
@@ -3,6 +3,7 @@ import { Trash2, Plus, Pencil } from "lucide-react";
 import { Button } from "./ui/button";
 import DataTable from "./common/DataTable";
 import { Student } from "@/types";
+import { Link } from "react-router-dom";
 
 
 const columns = [
@@ -105,16 +106,156 @@ const Students: React.FC = () => {
       class_id: 1,
       school_id: 1,
     },
+    {
+      id: 4,
+      first_name: "John",
+      last_name: "Doe",
+      dob: "2005-06-03",
+      pob: "Pointe-Noire",
+      gender: "M",
+      phone: "05 568 68 68",
+      address: "Siafoumou",
+      email: "john-doe@gmail.com",
+      guardian_name: "Jenathan Doe",
+      guardian_phone: "04 456 46 46",
+      class_id: 1,
+      school_id: 1,
+    },
+    {
+      id: 5,
+      first_name: "Daniel",
+      last_name: "Williams",
+      dob: "2005-04-13",
+      pob: "Pointe-Noire",
+      gender: "M",
+      phone: "05 568 68 68",
+      address: "Siafoumou",
+      email: "daniel-jones@gmail.com",
+      guardian_name: "Roger Williams",
+      guardian_phone: "04 456 46 46",
+      class_id: 1,
+      school_id: 1,
+    },
+    {
+      id: 6,
+      first_name: "Jane",
+      last_name: "Jones",
+      dob: "2005-09-23",
+      pob: "Pointe-Noire",
+      gender: "F",
+      phone: "05 568 68 68",
+      address: "Siafoumou",
+      email: "jane-jones@gmail.com",
+      guardian_name: "Sonia Lewis",
+      guardian_phone: "04 456 46 46",
+      class_id: 1,
+      school_id: 1,
+    },
+    {
+      id: 7,
+      first_name: "John",
+      last_name: "Doe",
+      dob: "2005-06-03",
+      pob: "Pointe-Noire",
+      gender: "M",
+      phone: "05 568 68 68",
+      address: "Siafoumou",
+      email: "john-doe@gmail.com",
+      guardian_name: "Jenathan Doe",
+      guardian_phone: "04 456 46 46",
+      class_id: 1,
+      school_id: 1,
+    },
+    {
+      id: 8,
+      first_name: "Daniel",
+      last_name: "Williams",
+      dob: "2005-04-13",
+      pob: "Pointe-Noire",
+      gender: "M",
+      phone: "05 568 68 68",
+      address: "Siafoumou",
+      email: "daniel-jones@gmail.com",
+      guardian_name: "Roger Williams",
+      guardian_phone: "04 456 46 46",
+      class_id: 1,
+      school_id: 1,
+    },
+    {
+      id: 9,
+      first_name: "Jane",
+      last_name: "Jones",
+      dob: "2005-09-23",
+      pob: "Pointe-Noire",
+      gender: "F",
+      phone: "05 568 68 68",
+      address: "Siafoumou",
+      email: "jane-jones@gmail.com",
+      guardian_name: "Sonia Lewis",
+      guardian_phone: "04 456 46 46",
+      class_id: 1,
+      school_id: 1,
+    },
+     {
+      id: 10,
+      first_name: "John",
+      last_name: "Doe",
+      dob: "2005-06-03",
+      pob: "Pointe-Noire",
+      gender: "M",
+      phone: "05 568 68 68",
+      address: "Siafoumou",
+      email: "john-doe@gmail.com",
+      guardian_name: "Jenathan Doe",
+      guardian_phone: "04 456 46 46",
+      class_id: 1,
+      school_id: 1,
+    },
+    {
+      id: 11,
+      first_name: "Daniel",
+      last_name: "Williams",
+      dob: "2005-04-13",
+      pob: "Pointe-Noire",
+      gender: "M",
+      phone: "05 568 68 68",
+      address: "Siafoumou",
+      email: "daniel-jones@gmail.com",
+      guardian_name: "Roger Williams",
+      guardian_phone: "04 456 46 46",
+      class_id: 1,
+      school_id: 1,
+    },
+    {
+      id: 12,
+      first_name: "Jane",
+      last_name: "Jones",
+      dob: "2005-09-23",
+      pob: "Pointe-Noire",
+      gender: "F",
+      phone: "05 568 68 68",
+      address: "Siafoumou",
+      email: "jane-jones@gmail.com",
+      guardian_name: "Sonia Lewis",
+      guardian_phone: "04 456 46 46",
+      class_id: 1,
+      school_id: 1,
+    },
   ]);
 
   return (
     <div className="p-6">
       <div className="flex justify-between items-center mb-6">
-        <h2 className="text-3xl font-bold">Gestion des étudiants</h2>
-        <Button className="flex items-center gap-2">
-          <Plus size={16} />
+        <h2 className="text-3xl font-semibold font-fredoka text-teal-400">
+          Gestion des étudiants
+        </h2>
+        <Link
+          to={"/"}
+          className="flex items-center gap-2 bg-gradient-to-tr from-teal-400 to-teal-500 px-3 py-2 rounded-md shadow-sm text-white dark:text-gray-950 font-semibold hover:scale-[1.02] transition-all"
+        >
+          <Plus className="font-semibold" size={16} />
           Ajouter un étudiant
-        </Button>
+        </Link>
       </div>
 
       <DataTable data={students} columns={columns} filters={filters} />
