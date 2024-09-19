5bb2cd92814a999bd196f1104a3584462544e609
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-19 17:52:04 +0100

    Update StudentPage.tsx

--- src/pages/modules/students/StudentPage.tsx
+++ src/pages/modules/students/StudentPage.tsx
@@ -13,7 +13,7 @@ const StudentPage: React.FC = () => {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <div className="flex-1 flex flex-col px-3 py-2 bg-slate-50 dark:bg-gray-800 ml-64">
+      <div className="flex-1 flex flex-col px-3 py-2 bg-slate-50 dark:bg-gray-900 ml-64">
         <Navbar />
         <main className="flex-1  mt-4">
           <DynamicBreadcrumb items={breadcrumbItems} />
