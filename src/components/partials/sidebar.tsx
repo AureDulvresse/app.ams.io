829fa8664ee7692ff6a21d7fc69a82fad5c00b1a
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-19 11:27:12 +0100

    sidebar updated

--- src/components/partials/sidebar.tsx
+++ src/components/partials/sidebar.tsx
@@ -1,17 +1,25 @@
 import React from "react";
-import logo from "/logo.png";
+import logo_light from "/logo_light.png";
+import logo_dark from "/logo_dark.png";
 import { useLocation } from "react-router-dom";
 import { routes } from "@/routes/router";
 import NavLink from "../common/NavLink";
+import { useTheme } from "@/hooks/theme-provider";
 
 const Sidebar: React.FC = () => {
   const location = useLocation();
   const currentPath = location.pathname;
 
+  const { theme } = useTheme();
+
   return (
     <div className="fixed top-0 left-0 w-64 h-screen bg-white dark:bg-gray-900 shadow-lg z-50">
       <div className="flex items-center gap-4 border-b p-4">
-        <img src={logo} className="w-20 h-auto" alt="logo" />
+        <img
+          src={theme === "light" ? logo_light : logo_dark}
+          className="w-20 h-auto"
+          alt="logo"
+        />
         <h1 className="text-base font-bold text-purple-400 font-fredoka">
           Academia Management Sync
         </h1>
