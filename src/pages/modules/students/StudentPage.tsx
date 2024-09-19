1748a4d040e98ca7eca117d448944b843ae378f2
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-19 11:50:12 +0100

    Update StudentPage.tsx

--- src/pages/modules/students/StudentPage.tsx
+++ src/pages/modules/students/StudentPage.tsx
@@ -15,7 +15,7 @@ const StudentPage: React.FC = () => {
       <Sidebar />
       <div className="flex-1 flex flex-col px-3 py-2 bg-slate-50 dark:bg-gray-800 ml-64">
         <Navbar />
-        <main className="flex-1">
+        <main className="flex-1  mt-4">
           <DynamicBreadcrumb items={breadcrumbItems} />
           <Students />
         </main>
