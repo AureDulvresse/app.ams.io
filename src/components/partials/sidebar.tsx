1167dc6e815139ce4cef31ecb4a8300efd98df49
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-19 11:28:24 +0100

    Update sidebar.tsx

--- src/components/partials/sidebar.tsx
+++ src/components/partials/sidebar.tsx
@@ -20,7 +20,7 @@ const Sidebar: React.FC = () => {
           className="w-20 h-auto"
           alt="logo"
         />
-        <h1 className="text-base font-bold text-purple-400 font-fredoka">
+        <h1 className="text-base font-bold text-purple-500 font-fredoka">
           Academia Management Sync
         </h1>
       </div>
