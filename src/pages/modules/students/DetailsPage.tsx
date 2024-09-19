0f1be55acf79f126c23422e251da7727dcc77409
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-19 11:44:45 +0100

    Update DetailsPage.tsx

--- src/pages/modules/students/DetailsPage.tsx
+++ src/pages/modules/students/DetailsPage.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import Navbar from "@/components/partials/Navbar";
 import Sidebar from "@/components/partials/Sidebar";
 import StudentDetail from "@/components/modules/students/StudentDetail";
+import DynamicBreadcrumb from "@/components/common/DynamicBreadcrumb";
 
+
+// Définir les éléments de fil d'Ariane pour cette page
+const breadcrumbItems = [
+  { href: "/", label: "Home" },
+  { href: "/students", label: "Students" },
+  { label: "Student Detail", isCurrent: true },
+];
 
 const StudentDetailPage: React.FC = () => {
   return (
@@ -10,7 +18,8 @@ const StudentDetailPage: React.FC = () => {
       <Sidebar />
       <div className="flex-1 flex flex-col px-3 py-2 bg-slate-50 dark:bg-gray-800 ml-64">
         <Navbar />
-        <main className="flex-1">
+        <main className="flex-1 mt-4">
+          <DynamicBreadcrumb items={breadcrumbItems} />
           <StudentDetail />
         </main>
       </div>
