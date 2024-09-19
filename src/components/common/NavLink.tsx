ef625951e47e3988213e499c4e894413c1d66b92
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-19 11:33:42 +0100

    Update NavLink.tsx

--- src/components/common/NavLink.tsx
+++ src/components/common/NavLink.tsx
@@ -14,22 +14,22 @@ const NavLink: React.FC<NavLinkProps> = ({ name,  icon: Icon, link, isActive }) 
       to={link}
       className={`mb-1.5 rounded-lg px-1.5 py-3 flex items-center gap-4 transition-colors ${
         isActive
-          ? "bg-purple-100 dark:bg-purple-600 text-purple-600 dark:text-purple-100 shadow-sm"
+          ? "bg-purple-200 dark:bg-purple-600 text-purple-600 dark:text-purple-100 shadow-sm"
           : "hover:bg-purple-50 dark:hover:bg-purple-700"
       }`}
     >
       <div
         className={`shadow-lg rounded-lg p-2 ${
           isActive
-            ? "bg-gradient-to-tr from-purple-400 to-purple-500"
-            : "bg-gradient-to-tr from-purple-300 to-purple-400"
+            ? "bg-gradient-to-tr from-purple-500 to-purple-600"
+            : "bg-gradient-to-tr from-purple-400 to-purple-500"
         }`}
       >
         <Icon className="text-white" />
       </div>
       <span
         className={`font-semibold text-base ${
-          isActive ? "text-purple-600 dark:text-purple-100" : "text-gray-700 dark:text-white"
+          isActive ? "text-purple-600 dark:text-purple-50" : "text-gray-700 dark:text-white"
         }`}
       >
         {name}
