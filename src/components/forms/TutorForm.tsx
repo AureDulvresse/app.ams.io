e296da3f010d2c49c81ecc9dc3cb5d08e39724ca
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-19 22:26:20 +0100

    Update TutorForm.tsx

--- src/components/forms/TutorForm.tsx
+++ src/components/forms/TutorForm.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { StudentTutor } from "@/types";
+import { Input } from "@/components/ui/input";
 
-const TutorForm: React.FC> = () => {
+const TutorForm: React.FC<StudentTutor> = ({ name, phone, email = "" }) => {
   return (
-    <div>TutorForm</div>
-  )
-}
+    <div className="flex flex-col gap-3">
+      <Input
+        name="name"
+        className="w-full"
+        placeholder="Entrer le nom complet du tuteur"
+        value={name ?? null}
+      />
+      <Input
+        type="tel"
+        name="phone"
+        placeholder="Entrer le numéro de téléphone du tuteur"
+        value={phone ?? null}
+      />
+      <Input
+        name="email"
+        placeholder="Entrer l'addresse mail du tuteur"
+        value={email ?? null}
+      />
+    </div>
+  );
+};
 
-export default TutorForm
+export default TutorForm;
