e681c6814df7056213c5d4bafae36c50e3177c93
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-19 11:50:29 +0100

    Update DashboardPage.tsx

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -10,7 +10,7 @@ const DashboardPage : React.FC = () => {
       <Sidebar />
       <div className="flex-1 flex flex-col px-3 py-2 bg-slate-50 dark:bg-gray-800 ml-64">
         <Navbar />
-        <main className="flex-1">
+        <main className="flex-1 mt-4">
           <Dashboard />
         </main>
       </div>
