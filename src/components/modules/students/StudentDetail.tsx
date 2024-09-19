8687cd75b6664809d8c12da21a2fad7a6399edcc
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-19 19:04:56 +0100

    Update StudentDetail.tsx

--- src/components/modules/students/StudentDetail.tsx
+++ src/components/modules/students/StudentDetail.tsx
@@ -2,7 +2,8 @@ import { Card } from "@/components/ui/card";
 import { Student } from "@/types";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { User, Mail, Phone } from "lucide-react";
+import { Mail, User2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 // Exemple de données fictives conformes à l'interface Student
 const exampleStudent = {
@@ -22,7 +23,7 @@ const exampleStudent = {
   school_id: 202,
 };
 
-const StudentDetailPage : React.FC = () => {
+const StudentDetailPage: React.FC = () => {
   const { studentId } = useParams();
   const [student, setStudent] = useState<Student>();
   const [loading, setLoading] = useState(true);
@@ -61,57 +62,60 @@ const StudentDetailPage : React.FC = () => {
   return (
     <div className="max-w-5xl mx-auto p-6 space-y-6">
       {/* Student Profile Header */}
-      <Card className="flex items-center p-6 space-x-4 bg-gradient-to-r from-purple-500 to-purple-600 text-white shadow-lg rounded-lg">
-        <img
-          src={student.picture}
-          alt={`${student.first_name} ${student.last_name} Profile`}
-          className="w-24 h-24 rounded-full shadow-md"
-        />
-        <div>
-          <h1 className="text-3xl font-bold">
-            {student.first_name} {student.last_name}
-          </h1>
-          <p className="text-lg">{student.gender}</p>
-          <p>Classe ID : {student.class_id}</p>
-          <p>École ID : {student.school_id}</p>
+      <Card className="flex justify-between p-6 bg-gradient-to-r from-purple-500 to-purple-600 text-white shadow-lg rounded-lg">
+        <div className="flex items-center space-x-4">
+          <img
+            src={student.picture}
+            alt={`${student.first_name} ${student.last_name} Profile`}
+            className="w-24 h-24 rounded-full shadow-md"
+          />
+          <div>
+            <h1 className="text-2xl font-bold">
+              {student.first_name} {student.last_name}
+            </h1>
+            <p className="text-lg">{student.gender}</p>
+            <p>Classe ID : {student.class_id}</p>
+            <p>École ID : {student.school_id}</p>
+          </div>
+        </div>
+
+        <div className="flex items-center space-x-2">
+          <Button>Modifier</Button>
         </div>
       </Card>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {/* Informations Personnelles */}
         <Card className="p-6 bg-white dark:bg-gray-900 shadow-lg rounded-lg">
-          <h2 className="flex items-center text-xl font-semibold mb-4">
-            <User className="text-purple-500 mr-2" /> Informations Personnelles
+          <h2 className="flex items-center font-bold mb-4 text-purple-500">
+            <User2 className=" mr-2" /> Informations Personnelles
           </h2>
           <ul className="space-y-2">
             <li>
-              <strong>Date de Naissance :</strong> {student.dob}
+              <strong className="text-sm">Date de Naissance :</strong>{" "}
+              {student.dob}
             </li>
             <li>
-              <strong>Lieu de Naissance :</strong> {student.pob}
+              <strong className="text-sm">Lieu de Naissance :</strong>{" "}
+              {student.pob}
             </li>
             <li>
-              <strong>Adresse :</strong> {student.address}
+              <strong className="text-sm">Adresse :</strong> {student.address}
             </li>
           </ul>
         </Card>
 
         {/* Informations de Contact */}
         <Card className="p-6 bg-white dark:bg-gray-900 shadow-lg rounded-lg">
-          <h2 className="flex items-center text-xl font-semibold mb-4">
-            <Mail className="text-purple-500 mr-2" /> Informations de Contact
+          <h2 className="flex items-center font-bold mb-4 text-purple-500">
+            <Mail className="mr-2" /> Informations de Contact
           </h2>
           <ul className="space-y-2">
             <li>
-              <strong>
-                <Phone className="inline-block mr-1" /> Téléphone :
-              </strong>{" "}
-              {student.phone}
+              <strong className="text-sm">Téléphone :</strong> {student.phone}
             </li>
             <li>
-              <strong>
-                <Mail className="inline-block mr-1" /> Email :
-              </strong>{" "}
+              <strong className="text-sm">Email :</strong>
               {student.email}
             </li>
           </ul>
@@ -119,17 +123,16 @@ const StudentDetailPage : React.FC = () => {
 
         {/* Informations du Tuteur */}
         <Card className="md:col-span-2 p-6 bg-white dark:bg-gray-900 shadow-lg rounded-lg">
-          <h2 className="flex items-center text-xl font-semibold mb-4">
-            <User className="text-purple-500 mr-2" /> Informations du Tuteur
+          <h2 className="flex items-center font-bold mb-4 text-purple-600">
+            <User2 className="mr-2" /> Informations du Tuteur
           </h2>
           <ul className="space-y-2">
             <li>
-              <strong>Nom du Tuteur :</strong> {student.guardian_name}
+              <strong className="text-sm">Nom du Tuteur :</strong>
+              {student.guardian_name}
             </li>
             <li>
-              <strong>
-                <Phone className="inline-block mr-1" /> Téléphone du Tuteur :
-              </strong>{" "}
+              <strong className="text-sm">Téléphone du Tuteur :</strong>
               {student.guardian_phone}
             </li>
           </ul>
