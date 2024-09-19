d27f2e773c433791576c4da76300a19ca3f815f7
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-19 19:31:23 +0100

    Update Dashboard.tsx

--- src/components/modules/Dashboard.tsx
+++ src/components/modules/Dashboard.tsx
@@ -12,7 +12,7 @@ import StatCard from "../common/StatCard";
 const Dashboard: React.FC = () => {
   return (
     <div className="p-6">
-      <h2 className="text-3xl font-bold mb-6">Overview</h2>
+      <h2 className="text-3xl font-bold font-fredoka text-indigo-500 mb-6">Overview</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {/* Student count */}
