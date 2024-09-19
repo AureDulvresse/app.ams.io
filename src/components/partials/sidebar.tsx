e0731c24f6b2c0b6b7be29a8133bec2ced4f377c
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-19 10:55:02 +0100

    Update sidebar.tsx

--- src/components/partials/sidebar.tsx
+++ src/components/partials/sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar: React.FC = () => {
     <div className="fixed top-0 left-0 w-64 h-screen bg-white dark:bg-gray-900 shadow-lg z-50">
       <div className="flex items-center gap-4 border-b p-4">
         <img src={logo} className="w-20 h-auto" alt="logo" />
-        <h1 className="text-base font-bold text-teal-400 font-fredoka">
+        <h1 className="text-base font-bold text-purple-400 font-fredoka">
           Academia Management Sync
         </h1>
       </div>
