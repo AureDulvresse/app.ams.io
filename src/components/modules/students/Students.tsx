cb4c022b54b3acf4084f5f14daff17fb88e96bdd
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-20 12:16:26 +0100

    Update Students.tsx

--- src/components/modules/students/Students.tsx
+++ src/components/modules/students/Students.tsx
@@ -245,7 +245,7 @@ const Students: React.FC = () => {
           Gestion des étudiants
         </h2>
         <Link
-          to={"/"}
+          to={"/students/create"}
           className="font-inter flex items-center gap-2 bg-gradient-to-tr from-indigo-400 to-indigo-500 px-3 py-2 rounded-md shadow-sm text-white dark:text-gray-950 font-semibold hover:scale-[1.02] transition-all"
         >
           <Plus className="font-semibold" size={16} />
