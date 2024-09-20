90abf7ad23c0de995b032d019a861817219cc457
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-20 11:43:17 +0100

    .

---
+++ src/components/forms/PersonalInfoForm.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "../ui/input";
+import { Select } from "../ui/select";
+
+interface PersonalInfoFormProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  formData: any;
+  handleInputChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => void;
+  nextStep: () => void;
+}
+
+const PersonalInfoForm: React.FC<PersonalInfoFormProps> = ({
+  formData,
+  handleInputChange,
+  nextStep,
+}) => {
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">
+        Étape 1 : Informations Personnelles
+      </h2>
+      <div className="space-y-4">
+        <Input
+          type="text"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleInputChange}
+          placeholder="Prénom"
+          className="Input"
+        />
+        <Input
+          type="text"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleInputChange}
+          placeholder="Nom"
+        />
+        <Input
+          type="date"
+          name="dob"
+          value={formData.dob}
+          onChange={handleInputChange}
+        />
+        <Input
+          type="text"
+          name="pob"
+          value={formData.pob}
+          onChange={handleInputChange}
+          placeholder="Lieu de Naissance"
+        />
+        <Input
+          type="text"
+          name="address"
+          value={formData.address}
+          onChange={handleInputChange}
+          placeholder="Adresse"
+        />
+        <Input
+          type="tel"
+          name="phone"
+          value={formData.phone}
+          onChange={handleInputChange}
+          placeholder="Téléphone"
+        />
+        <Input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          placeholder="Email"
+        />
+        <Select
+          name="gender"
+          value={formData.gender}
+          onChange={handleInputChange}
+          className="Input"
+        >
+          <option value="" disabled>
+            Sexe
+          </option>
+          <option value="Masculin">Masculin</option>
+          <option value="Féminin">Féminin</option>
+        </Select>
+      </div>
+      <Button className="mt-4 bg-indigo-500 text-white" onClick={nextStep}>
+        Suivant
+      </Button>
+    </div>
+  );
+};
+
+export default PersonalInfoForm;
