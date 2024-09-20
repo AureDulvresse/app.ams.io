f0fbfd82d9aca3047041d418688bee2c8aa63076
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-20 15:12:58 +0100

    Update PersonalInfoForm.tsx

--- src/components/forms/PersonalInfoForm.tsx
+++ src/components/forms/PersonalInfoForm.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import SelectField from "../common/SelectField";
+import { Calendar } from "../ui/calendar";
+import { format } from "date-fns";
 
 interface PersonalInfoFormProps {
   formData: {
     firstName: string;
     lastName: string;
-    dob: string;
+    dob: string | Date;
     pob: string;
     address: string;
     phone: string;
@@ -30,10 +32,21 @@ const PersonalInfoForm: React.FC<PersonalInfoFormProps> = ({
   handleInputChange,
   nextStep,
 }) => {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
+
   const handleGenderChange = (value: string) => {
     handleInputChange({
-      target: { name: "gender", value }, // Directement utiliser la valeur
+      target: { name: "gender", value },
     } as unknown as React.ChangeEvent<HTMLSelectElement>);
+  };
+
+  const handleDateChange = (value: Date | undefined) => {
+    if (value) {
+      setDate(value);
+      handleInputChange({
+        target: { name: "dob", value: format(value, "yyyy-MM-dd") }, // Format date for the input
+      } as unknown as React.ChangeEvent<HTMLSelectElement>);
+    }
   };
 
   return (
@@ -58,13 +71,21 @@ const PersonalInfoForm: React.FC<PersonalInfoFormProps> = ({
           placeholder="Nom"
           required
         />
-        <Input
-          type="date"
-          name="dob"
-          value={formData.dob}
-          onChange={handleInputChange}
-          required
-        />
+
+        <div className="flex flex-col">
+          <label className="mb-2">Date de Naissance</label>
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={handleDateChange}
+            className="rounded-md border shadow"
+            disabled={(date) =>
+              date > new Date() || date < new Date("1900-01-01")
+            }
+            initialFocus
+          />
+        </div>
+
         <Input
           type="text"
           name="pob"
@@ -101,7 +122,7 @@ const PersonalInfoForm: React.FC<PersonalInfoFormProps> = ({
           label="Sexe"
           placeholder="Sélectionner le sexe"
           options={genders}
-          onChange={handleGenderChange} // Passer directement la fonction
+          onChange={handleGenderChange}
         />
       </div>
       <Button className="mt-4 bg-indigo-500 text-white" onClick={nextStep}>
