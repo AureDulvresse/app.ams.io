982bcb9452d28505b752e707510d5730c0ade294
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-09-23 12:45:15 +0100

    Update AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,36 +1,28 @@
+import api from "@/lib/api";
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
 
-type AuthContextType = {
+interface AuthContextType {
   isAuthenticated: boolean;
-  login: (
-    email: string,
-    password: string,
-    rememberMe: boolean
-  ) => Promise<void>;
+  login:  (email: string, password: string, rememberMe: boolean) => Promise<void>;
   logout: () => void;
-  token: string | null;
-};
+}
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-  const [token, setToken] = useState<string | null>(null);
-  const navigate = useNavigate();
-
-  // Vérifier le token au chargement initial
-  useEffect(() => {
-    const storedToken =
-      localStorage.getItem("token") || sessionStorage.getItem("token");
-    if (storedToken) {
-      setToken(storedToken);
-      setIsAuthenticated(true);
-    }
-  }, []);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [token, setToken] = useState<string | null>(null);
+    
+    useEffect(() => {
+      const storedToken =
+        localStorage.getItem("token") || sessionStorage.getItem("token");
+      if (storedToken) {
+        setToken(storedToken);
+        setIsAuthenticated(true);
+      }
+    }, []);
 
   const login = async (
     email: string,
@@ -39,40 +31,39 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   ) => {
     try {
       // Remplacez l'URL par celle de votre endpoint d'authentification
-      const response = await axios.post("https://votre-api.com/login", {
+      const response = await api.post("/login/", {
         email,
         password,
       });
 
-      const receivedToken = response.data.token; // Supposons que l'API retourne un token
+      setToken(response.data.token); // Supposons que l'API retourne un token
 
       // Stocker le token selon le choix de l'utilisateur
       if (rememberMe) {
-        localStorage.setItem("token", receivedToken);
+        localStorage.setItem("token", token??"");
       } else {
-        sessionStorage.setItem("token", receivedToken);
+        sessionStorage.setItem("token", token??"");
       }
 
-      setToken(receivedToken);
       setIsAuthenticated(true);
-      navigate("/"); // Rediriger vers la page d'accueil
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
+      
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+    } catch (error : any) {
       // Gérer les erreurs d'authentification
-      throw new Error(`Email ou mot de passe incorrect ${error.message}`);
+      throw new Error("Email ou mot de passe incorrect");
     }
   };
 
-  const logout = () => {
-    setIsAuthenticated(false);
-    setToken(null);
-    localStorage.removeItem("token");
-    sessionStorage.removeItem("token");
-    navigate("/login"); // Rediriger vers la page de connexion
-  };
+ const logout = () => {
+   setIsAuthenticated(false);
+   setToken(null);
+   localStorage.removeItem("token");
+   sessionStorage.removeItem("token");
+ };
+
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout, token }}>
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
@@ -81,8 +72,8 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
 // eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => {
   const context = useContext(AuthContext);
-  if (context === undefined) {
-    throw new Error("useAuth doit être utilisé à l'intérieur de AuthProvider");
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
   }
   return context;
 };
